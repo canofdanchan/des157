ad466e84e9dc4cf117c2fe945fc85ebe0b4ecd3b
Dana Chan <danadlchan@gmail.com>
2018-01-18 09:31:44 -0800

    Initial commit
    Add portal site.

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log ("this is a script.js comment");
+
+function setup(){
+  var myCanvas = createCanvas (800,250);
+  myCanvas.parent ('my-sketch');
+  background(255, 229, 243);
+}
+
+function draw(){
+  stroke (175, 135, 231);
+  strokeWeight(3);
+  line (mouseX,mouseY,pmouseX,pmouseY);
+
+}
